06829c33bebb3037eedfe3c6212fa7906efc86c6
Conrad Parker <conrad@metadecks.org>
2014-10-13 22:36:17 -0700

    chmod: Add symbolic designators
    Supports comma-separated ([ugoa][=+-]([rwx]*|[ugo]))*

--- src/Chmod.hs
+++ src/Chmod.hs
@@ -1,19 +1,34 @@
 module Chmod (
       Chmod(..)
+    , ChmodWho(..)
+    , ChmodWhat(..)
     , parseChmod
     , applyChmod
     ) where
 
-import           Control.Applicative ((<$>))
-import           Control.Monad (guard)
+import           Control.Applicative
+import           Control.Monad (guard, msum)
 import qualified Data.Attoparsec.ByteString.Char8 as Atto
 import qualified Data.Attoparsec.Combinator as Atto
+import           Data.Bits
 import qualified Data.ByteString.Char8 as B
 import           Data.Char (ord)
 import           Data.List (foldl')
 import           Data.Word (Word16, Word32)
 
-data Chmod = SetMode Word16
+data ChmodWho = Chmod_u | Chmod_g | Chmod_o | Chmod_a 
+    deriving (Show, Eq)
+data ChmodWhat = Chmod_r | Chmod_w | Chmod_x | Chmod_X
+    | Chmod_s | Chmod_t
+    deriving (Show, Eq)
+
+data Chmod = SetOctal    Word16
+           | SetEqual    ChmodWho [ChmodWhat]
+           | SetPlus     ChmodWho [ChmodWhat]
+           | SetMinus    ChmodWho [ChmodWhat]
+           | SetEqualWho ChmodWho ChmodWho
+           | SetPlusWho  ChmodWho ChmodWho
+           | SetMinusWho ChmodWho ChmodWho
     deriving (Show, Eq)
 
 parseChmod :: Atto.Parser [Chmod]
@@ -22,7 +37,40 @@ parseChmod = do
     guard (cs /= [])
     return cs
   where
-    chmod1 = SetMode <$> octal
+    chmod1 = msum
+        [ SetOctal <$> octal
+        , SetEqual <$> ugoa <* Atto.char '=' <*> Atto.many1 rwx
+        , SetPlus  <$> ugoa <* Atto.char '+' <*> Atto.many1 rwx
+        , SetMinus <$> ugoa <* Atto.char '-' <*> Atto.many1 rwx
+        , SetEqualWho <$> ugoa <* Atto.char '=' <*> ugo
+        , SetPlusWho  <$> ugoa <* Atto.char '+' <*> ugo
+        , SetMinusWho <$> ugoa <* Atto.char '-' <*> ugo
+        ]
+
+    ugo :: Atto.Parser ChmodWho
+    ugo = msum
+        [ pure Chmod_u <* Atto.char 'u'
+        , pure Chmod_g <* Atto.char 'g'
+        , pure Chmod_o <* Atto.char 'o'
+        ]
+
+    ugoa :: Atto.Parser ChmodWho
+    ugoa = msum
+        [ ugo
+        , pure Chmod_a <* Atto.char 'a'
+        ]
+
+    rwx :: Atto.Parser ChmodWhat
+    rwx = msum
+        [ pure Chmod_r <* Atto.char 'r'
+        , pure Chmod_w <* Atto.char 'w'
+        , pure Chmod_x <* Atto.char 'x'
+        {-
+        , pure Chmod_X <* Atto.char 'X'
+        , pure Chmod_s <* Atto.char 's'
+        , pure Chmod_t <* Atto.char 't'
+        -}
+        ]
 
     octal :: Atto.Parser Word16
     octal = B.foldl' step 0 `fmap` Atto.takeWhile1 isDig
@@ -34,4 +82,47 @@ applyChmod :: [Chmod] -> Word16 -> Word16
 applyChmod cs old = foldl' f old cs
   where
     f :: Word16 -> Chmod -> Word16
-    f _ (SetMode new) = new
+    f _   (SetOctal new)        = new
+    f old (SetEqual who ws)     = set who old (foldRWX ws)
+    f old (SetPlus  who ws)     = plus who old (foldRWX ws)
+    f old (SetMinus who ws)     = minus who old (foldRWX ws)
+    f old (SetEqualWho who src) = set who old (extract src old)
+    f old (SetPlusWho who src)  = plus who old (extract src old)
+    f old (SetMinusWho who src) = minus who old (extract src old)
+
+    set :: ChmodWho -> Word16 -> Word16 -> Word16
+    set who old new = (old .&. (complement (mask who))) .|.
+                      setWho who new
+
+    plus :: ChmodWho -> Word16 -> Word16 -> Word16
+    plus who old new = old .|. setWho who new
+
+    minus :: ChmodWho -> Word16 -> Word16 -> Word16
+    minus who old new = old `xor` setWho who new
+
+    foldRWX = foldl' (\old what -> old .|. b what) 0
+
+    mask :: ChmodWho -> Word16
+    mask Chmod_a = 777
+    mask who     = 7 `shiftL` s who
+
+    setWho :: ChmodWho -> Word16 -> Word16
+    setWho Chmod_a new = foldl' (.|.) 0 $
+        map (\w -> setWho w new) [Chmod_u, Chmod_g, Chmod_o]
+    setWho who new = new `shiftL` s who
+
+    extract :: ChmodWho -> Word16 -> Word16
+    extract who old = (mask who .&. old) `shiftR` s who
+
+    -- Bit to set for what
+    b :: ChmodWhat -> Word16
+    b Chmod_r = 4
+    b Chmod_w = 2
+    b Chmod_x = 1
+
+    -- Number of bits to shift for who
+    s :: ChmodWho -> Int
+    s Chmod_u = 6
+    s Chmod_g = 3
+    s Chmod_o = 0
+
--- tests/test.hs
+++ tests/test.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main where
+module Main where 
 
 import           Test.Tasty
 import           Test.Tasty.HUnit
@@ -24,10 +24,48 @@ tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
 unitTests = testGroup "Unit tests"
-  [ testChmod "755" [SetMode 493]
-  , testChmod "0755" [SetMode 493]
+  [ testChmod "755"  [SetOctal 493]
+  , testChmod "0755" [SetOctal 493]
+  , testChmod "u=r"  [SetEqual Chmod_u [Chmod_r]]
   , testChmodShouldFail "kittens"
+  
+  , testApplyChmod [SetEqual    Chmod_o [Chmod_r]]
+        (oct 0 0 0) (oct 0 0 4)
+  , testApplyChmod [SetEqual    Chmod_o [Chmod_r, Chmod_w]]
+        (oct 0 0 0) (oct 0 0 6)
+  , testApplyChmod [SetEqual    Chmod_g [Chmod_r]]
+        (oct 0 0 0) (oct 0 4 0)
+  , testApplyChmod [SetPlus     Chmod_o [Chmod_w]]
+        (oct 0 0 4) (oct 0 0 6)
+  , testApplyChmod [SetPlus     Chmod_o [Chmod_w,Chmod_r]]
+        (oct 0 0 1) (oct 0 0 7)
+  , testApplyChmod [SetMinus    Chmod_o [Chmod_w]]
+        (oct 0 0 6) (oct 0 0 4)
+  , testApplyChmod [SetMinus    Chmod_o [Chmod_w,Chmod_r]]
+        (oct 0 0 7) (oct 0 0 1)
+  , testApplyChmod [SetEqualWho Chmod_g Chmod_o]
+        (oct 0 0 1) (oct 0 1 1)
+  , testApplyChmod [SetPlusWho  Chmod_g Chmod_o]
+        (oct 0 2 1) (oct 0 3 1)
+  , testApplyChmod [SetMinusWho Chmod_g Chmod_o]
+        (oct 0 3 1) (oct 0 2 1)
+  , testApplyChmod [SetMinusWho Chmod_g Chmod_o]
+        (oct 0 3 1) (oct 0 2 1)
+  , testApplyChmod [SetEqual    Chmod_a [Chmod_r]]
+        (oct 0 0 0) (oct 4 4 4)
+  , testApplyChmod [SetPlus     Chmod_a [Chmod_r]]
+        (oct 0 1 0) (oct 4 5 4)
+  , testApplyChmod [SetMinus    Chmod_a [Chmod_x]]
+        (oct 7 5 5) (oct 6 4 4)
+  , testApplyChmod [SetEqualWho Chmod_a Chmod_u]
+        (oct 6 4 4) (oct 6 6 6)
+  , testApplyChmod [SetPlusWho  Chmod_a Chmod_u]
+        (oct 7 5 5) (oct 7 7 7)
+  , testApplyChmod [SetMinusWho Chmod_a Chmod_o]
+        (oct 7 5 5) (oct 2 0 0)
   ]
+
+oct u g o = u*64 + g*8 + o
 
 testChmod :: ByteString -> [Chmod] -> TestTree
 testChmod input expected =
@@ -36,6 +74,12 @@ testChmod input expected =
 
 testChmodShouldFail :: ByteString -> TestTree
 testChmodShouldFail input =
-    testCase (unwords ["Parse chmod", B.unpack input, "(should fail)"]) $
+    testCase (unwords ["Parse chmod", B.unpack input]) $
         assertBool "Failed to catch invalid chmod"
         (isLeft $ parseOnly parseChmod input)
+
+testApplyChmod :: [Chmod] -> Word16 -> Word16 -> TestTree
+testApplyChmod input old expected =
+    testCase (unwords ["Apply chmod", show input, "to", show old]) $
+        applyChmod input old @?= expected
+
